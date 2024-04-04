93c348b22389cace44958a879661e3ec06cad2ac
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2024-04-04 13:01:19 +0300

    remove old build pipeline

--- build.bat
+++
@@ -1,6 +0,0 @@
-@mkdir obj
-@mkdir src\lib
-@mkdir build
-@git clone --branch 2.5.1 https://github.com/SFML/SFML.git
-@cmake -G "MinGW Makefiles" -S SFML -B build
-@cd build && mingw32-make
--- install.bat
+++
@@ -1,6 +0,0 @@
-@move build\lib\*.a src\lib
-@move build\lib\*.dll .
-@move SFML\include\SFML src/include
-@move SFML\extlibs\bin\x64\openal32.dll .
-@rd /s /q build
-@rd /s /q SFML
